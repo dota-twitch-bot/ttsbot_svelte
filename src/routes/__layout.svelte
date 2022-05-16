7c32ba2169c523db774039ab71749256262ed4b0
bl00dshooter <bl00dshootermail@gmail.com>
2022-05-16 16:33:14 -0300

    adicionado versão ao footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import '../app.css';
+	import { version } from '../current_version.js';
 	// var port = chrome.runtime.connect("mjnfalinkaablgiaknbjjiiophgeiape");
 
 	// window.addEventListener(
@@ -21,4 +22,14 @@
 
 <div class="grid place-items-center">
 	<slot />
+	<div id="footer" class="fixed bottom-0 border-gray-400 border-t-2 w-full text-center">
+		<p><span class="font-bold">Versão:</span>
+			{#if version}
+				{version}
+			{:else }
+				Indefinida
+			{/if}
+		</p>
+		<p><span class="font-bold">Discord para suporte:</span> <a href="https://discord.gg/qM2nZ2jMM2" class="text-blue-500">https://discord.gg/qM2nZ2jMM2</a></p>
+	</div>
 </div>
