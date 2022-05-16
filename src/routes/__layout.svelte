de5ecca4ee5d24739d6a3f9af4455d4fba792146
bl00dshooter <bl00dshootermail@gmail.com>
2022-05-16 17:08:27 -0300

    Adicionado titulo à pagina

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,23 +1,6 @@
 <script>
 	import '../app.css';
 	import { version } from '../current_version.js';
-	// var port = chrome.runtime.connect("mjnfalinkaablgiaknbjjiiophgeiape");
-
-	// window.addEventListener(
-	// 	'message',
-	// 	(event) => {
-	// 		// We only accept messages from ourselves
-	// 		if (event.source != window) {
-	// 			return;
-	// 		}
-
-	// 		if (event.data.type && event.data.type == 'FROM_PAGE') {
-	// 			console.log('Content script received: ' + event.data.text);
-	// 			port.postMessage(event.data.text);
-	// 		}
-	// 	},
-	// 	false
-	// );
 </script>
 
 <div class="grid place-items-center">
