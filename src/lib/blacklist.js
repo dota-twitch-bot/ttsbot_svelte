e898222bdd51ddc56501c654172f8aa92a8d1ff4
bl00dshooter <bl00dshootermail@gmail.com>
2022-05-23 21:45:36 -0300

    fix

--- src/lib/blacklist.js
+++ src/lib/blacklist.js
@@ -3,10 +3,15 @@ headers.append('pragma', 'no-cache');
 headers.append('cache-control', 'no-cache');
 headers.append('cache-control', 'no-store');
 
-export const blacklist = JSON.parse(await fetch('https://ebpimenta.xyz/static/blacklist.json', {
+export const blacklist = JSON.parse(await fetch('https://ebpimenta.xyz/static/blacklist.jsona', {
     method: 'GET',
 }).then(response => {
     return response.text();
 }).then(text => {
     return text;
-}).catch(e => console.log(e))).users;
+}).catch(e => {
+    return JSON.stringify(new Object({
+        users: []
+    }))
+}
+)).users;
